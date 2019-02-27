2c286359c3de27da1cbb72db05275bec049b6da5
Paul Craig <paul_craig_16@hotmail.com>
2019-02-27 14:48:53 -0500

    Add page 3 and add a test for page 3

---
+++ pages/three.js
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+
+const Three = () => (
+  <div>
+    <h1>Page Three</h1>
+    <p>Way better than page 2, right?</p>
+    <Link href="/two">
+      <a>Back to Page Two</a>
+    </Link>
+  </div>
+)
+
+export default Three
