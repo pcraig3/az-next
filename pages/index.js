c1645c7b75a73afcd5318b04cd865a3c46cfc282
Paul Craig <paul_craig_16@hotmail.com>
2019-03-06 16:39:10 -0500

    Update timestamp on index page
    This will trigger our automatic builds

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ const Index = () => (
       <a>Page Two</a>
     </Link>
     <br />
-    <p>Last updated: Wed, 06 Mar 2019 19:20:52 GMT</p>
+    <p>Last updated: Wed, 06 Mar 2019 23:19:27 GMT</p>
   </div>
 )
 
